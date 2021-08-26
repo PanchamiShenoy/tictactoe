18685cbc263e5c2a17052090810fce816a2d7647
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-08-26 10:02:49 +0530

    calling of method

--- src/com/yml/tictac/TicTacToeGame.java
+++ src/com/yml/tictac/TicTacToeGame.java
@@ -4,10 +4,15 @@ public class TicTacToeGame {
 	
 	public static char[] board= new char[10];  
 	
+	public void createBoard() {
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to TicTacToe");
-		
+		TicTacToeGame t=new TicTacToeGame();
+		t.createBoard();
 	}
 
 }
